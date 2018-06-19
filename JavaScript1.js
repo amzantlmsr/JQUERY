d044c5f9ef73c9fbeaf152822e189accb260a37a
amzantlmsr <39835728+amzantlmsr@users.noreply.github.com>
2018-06-20 01:00:31 +0300

    Add files via upload

---
+++ JavaScript1.js
@@ -0,0 +1,34 @@
+﻿
+    var n = $("#todo-list li").length
+    $("p").text("There are " + n + " to do items.");
+
+    $('#add').click(function () {
+        let input = $('#element').val();
+
+        if (input == "") {
+        alert('not accepted empty action');
+    return false;
+}
+        else {
+        $('#todo-list').append($('<li class="list-item"></li>').text(input));
+    var n = $("#todo-list li").length
+    $("p").text("There are " + n + " to do items.");
+    }
+});
+
+        $('#todo-list').delegate('li', 'click', function () {
+            let selectedElement = $(this);
+        selectedElement.remove();
+        $('#done-list').append($('<li class="done-item"></li>').text(selectedElement.text()));
+        var n = $("#todo-list li").length
+        $("p").text("There are " + n + " to do items.");
+    });
+
+
+    $('#done-list').delegate('li', 'click', function () {
+            let selectedElementa = $(this);
+        selectedElementa.remove();
+        $('#todo-list').append($('<li class="list-item"></li>').text(selectedElementa.text()));
+        var n = $("#todo-list li").length
+        $("p").text("There are " + n + " to do items.");
+    });
